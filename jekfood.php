4c252897651daefec669dd399c4abbbf8c8022b3
megatruh <55190771+megatruh@users.noreply.github.com>
2019-11-18 22:04:55 +0700

    nambahi jeneng

--- jekfood.php
+++ jekfood.php
@@ -26,6 +26,20 @@ function fetch_value($str,$find_start,$find_end)
         return trim(substr($str,$start +$length,$end));
     }
 
+function nama()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $ex = curl_exec($ch);
+        // $rand = json_decode($rnd_get, true);
+        preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
+        return $name[2][mt_rand(0, 14) ];
+    }
+
 $secret = '83415d06-ec4e-11e6-a41b-6c40088ab51e';
 $headers = array();
 $headers[] = 'Content-Type: application/json';
