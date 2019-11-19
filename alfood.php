a22cb4601bd5ecd843414694169ab17d4c1f6285
megatruh <55190771+megatruh@users.noreply.github.com>
2019-11-19 11:02:15 +0700

    FIX Lebih Mantab Mantab

--- alfood.php
+++ alfood.php
@@ -10,6 +10,7 @@ $headers[] = 'X-Location: -6.405821,106.064193';
         echo "\n";
         echo "\e[92m--------------------------------------------------\n";
         echo "\e[92m|              SCRIPT BY ALFARKHAN               |\n";
+        echo "\e[92m|            Komunitas Tumbuh Bersama            |\n";
         echo "\e[92m|     https://github.com/megatruh/malproject/    |\n";
         echo "\e[92m|   SCRIPT GOJEK AUTO REGIST + AUTO SAVE TOKEN   |\n";
         echo "\e[92m|                    GOODLUCK                    |\n";
